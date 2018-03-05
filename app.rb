1c07349da6e8956ea61e27c4bbe9654def6fdecc
Antonio <ciniglioantonio@gmail.com>
2018-03-05 16:23:22 +0000

    Sinatra returns HTML (create CSS folder)

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'shotgun'
 
+set :public_folder, File.dirname(__FILE__) + '/static'
+
 get '/' do
   "Hello!"
 end
@@ -19,4 +21,13 @@ end
 
 get '/hugs' do
   "We give free hugs here"
+end
+
+get '/cat' do
+  #version with Chris Ly 05/03/2018
+  #"<img style='border-style: dotted; border-color: red' src='http://bit.ly/1eze8aE'>"
+  #walkthrough version
+  "<div style='border: 3px dashed red'>
+   <img src='http://bit.ly/1eze8aE'>
+ </div>"
 end
